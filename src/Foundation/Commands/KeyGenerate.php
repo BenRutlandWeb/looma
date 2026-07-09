8230a79ddef17466ca9e4e7aeb81c5df8b295482
Ben Rutland <ben@10degrees.uk>
2026-07-09 15:00:15 +0100

    changed key generate command

--- src/Foundation/Commands/KeyGenerate.php
+++ src/Foundation/Commands/KeyGenerate.php
@@ -18,48 +18,12 @@ final class KeyGenerate implements CommandInterface
      */
     public function __invoke(array $arguments = [], array $options = []): void
     {
-        // @todo key:generate sould and maybe should use aaemnnosttv/wp-cli-dotenv-command
-        // \Looma\Foundation\Commands\KeyGenerate::class,
-
         $this->header('Looma', 'Generate keys and salts for the application.');
 
-        $keys = [
-            'AUTH_KEY',
-            'SECURE_AUTH_KEY',
-            'LOGGED_IN_KEY',
-            'NONCE_KEY',
-            'AUTH_SALT',
-            'SECURE_AUTH_SALT',
-            'LOGGED_IN_SALT',
-            'NONCE_SALT',
-            'WP_CACHE_KEY_SALT',
-        ];
-
-        $filename = dirname(dirname(ABSPATH)) . '/.env';
-
-        if (!$this->exists($filename)) {
-            $this->error('.env file doesn\'t exist.');
+        if ($this->callSilently("dotenv salts regenerate")) {
+            $this->success("Generated application keys and salts.")->terminate();
         }
 
-        $env = $this->getContents($filename);
-
-        foreach ($keys as $key) {
-            $value = wp_generate_password(64, true, true);
-
-            $pattern = "/^($key=.*)$/m";
-
-            if (preg_match($pattern, $env)) {
-                $env = preg_replace($pattern, "$key=\"$value\"", $env);
-            } else {
-                $env .= "\n$key=\"$value\"";
-            }
-
-            $this->line("<info>$key</info> generated.");
-            usleep(100000);
-        }
-
-        $this->putContents($filename, $env);
-
-        $this->newLine()->success('All keys generated.', true);
+        $this->error('Failed to generate the application keys and salts.');
     }
 }
--- src/Foundation/FoundationServiceProvider.php
+++ src/Foundation/FoundationServiceProvider.php
@@ -24,8 +24,7 @@ final class FoundationServiceProvider implements ServiceProviderInterface
             \Looma\Foundation\Commands\ClearCompiled::class,
             \Looma\Foundation\Commands\EnvironmentGet::class,
             \Looma\Foundation\Commands\EnvironmentSet::class,
-            // @todo key:generate could and maybe should use aaemnnosttv/wp-cli-dotenv-command
-            // \Looma\Foundation\Commands\KeyGenerate::class,
+            \Looma\Foundation\Commands\KeyGenerate::class,
             \Looma\Foundation\Commands\ListCommands::class,
             \Looma\Foundation\Commands\MakeServiceProvider::class,
         ]);
